7be92c9d4937293ac53985c86240ea45913d65e2
ugesh <ugesh.roopchand@hotmail.com>
2022-04-09 11:17:06 +0400

    accordion btn

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -1,4 +1,7 @@
-
+$( document ).ready(function() {
+    accordion()
+    ShowAlloption();
+});
 
 import plan from "./data.js";
 
@@ -6,44 +9,65 @@ import plan from "./data.js";
 
 
 function accordion(){
-    let accorbtns = document.querySelectorAll(".accordion-btn");
-// let upicon = document.querySelector(".up-icon");
-// let downicon = document.querySelector(".down-icon");
+let accorbtns = document.querySelectorAll(".accordion-btn");
 
 if(accorbtns){
     accorbtns.forEach(function(btn){
         btn.addEventListener('click',function(e){
-          const accordion = e.currentTarget.parentElement.parentElement.parentElement;
-        //   console.log(accordion)
-         accordion.classList.toggle('show');
+            //const accordionselectedbtn = e.currentTarget;
+          const accordion = e.currentTarget.parentElement.parentElement;
+         console.log(accordion)
+         accordion.classList.toggle('show-text');
         })
     })
+}
+
+}
+
+function sidenav(){
+
+}
+
+
+
+function ShowAlloption(){
+
+    showoptions()
+}
+
+
+
+
+function showoptions(){
     
-}
 
-}
+    for(let i = 0; i < plan.length; i++){
+        console.log(plan[i]);
+        let options = plan[i];
+    
+        if(options.type == 'Preferences'){
+            for(let i = 0; i< options.details.length; i++ ){
+                console.log(options.details[i].name)
+                let cardinfo = options.details[i];
+                let preferencecards = document.querySelector('.preference__cards');
 
-let Preferencescontainer = document.querySelector('.preferencescontainer');
+                let preferencecardwrapper = document.createElement('div');
+                preferencecardwrapper.setAttribute('class','preferencecardwrapper');
+                preferencecards.appendChild(preferencecardwrapper);
 
-for(let i = 0; i < plan.length; i++){
-    console.log(plan[i]);
-    let options = plan[i];
+                let preferencecardwrapperinner = document.querySelector(".preferencecardwrapper");
 
-    if(options.type == 'Preferences'){
-        for(let i = 0; i< options.details.length; i++ ){
-            console.log(options.details[i].name)
-            let cardinfo = options.details[i];
-            let preferencecards = document.querySelector('.preference__cards');
-            let preferencecardoption = document.createElement('div');
-            preferencecardoption.setAttribute('id',`card_${cardinfo.id}`);
-            preferencecardoption.setAttribute('class','preference__cards__option');
-            preferencecards.appendChild(preferencecardoption);
-
-            let cardoptionid = document.getElementById(`card_${cardinfo.id}`);
-            cardoptionid.innerHTML =`<h4>${cardinfo.name}</h4>
-                                        <p>${cardinfo.description}</p>`;
+                let preferencecardoption = document.createElement('div');
+                preferencecardoption.setAttribute('id',`card_${cardinfo.id}`);
+                preferencecardoption.setAttribute('class','preference__cards__option');
+                preferencecardwrapperinner.appendChild(preferencecardoption);
+    
+                let cardoptionid = document.getElementById(`card_${cardinfo.id}`);
+                cardoptionid.innerHTML =`<h4 class="cardinfoname_${cardinfo.id}">${cardinfo.name}</h4>
+                                            <p>${cardinfo.description}</p>`;
+            }
         }
+    
+    
     }
-
-
 }
